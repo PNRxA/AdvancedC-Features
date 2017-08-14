45fa30b04e96e0f04afe7b595a804fc083951c7a
Peter Runham <pnrunham@gmail.com>
2017-08-14 16:02:29 +1000

    Added classes to namespace. Started work on shotgun script.

--- Assets/3 - Abstract Classes/Scripts/Movement.cs
+++ Assets/3 - Abstract Classes/Scripts/Movement.cs
@@ -2,15 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+namespace AbstractClasses
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Movement : MonoBehaviour
+    {
+        public float acceleration = 25f;
+        public float hyperSpeed = 150f;
+        public float deceleration = .1f;
+        public float rotationSpeed = 5f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private Rigidbody2D rigid;
+        private float inputH, inputV;
+
+        void Awake()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            inputH = Input.GetAxis("Horizontal");
+            inputV = Input.GetAxisRaw("Vertical");
+        }
+
+        void FixedUpdate()
+        {
+            Accelerate();
+            Decelerate();
+            Rotate();
+        }
+
+        void Accelerate()
+        {
+            Vector2 force = transform.right * inputV;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                force *= hyperSpeed;
+            }
+            else
+            {
+                force *= acceleration;
+            }
+            rigid.AddForce(force);
+        }
+
+        void Decelerate()
+        {
+            //velocity += -velocity * deceleration
+            rigid.velocity += -rigid.velocity * deceleration;
+        }
+
+        void Rotate()
+        {
+            transform.rotation *= Quaternion.AngleAxis(rotationSpeed * inputH, Vector3.back);
+        }
+    }
 }
